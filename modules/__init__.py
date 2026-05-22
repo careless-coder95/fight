8674e9a6643857c27e7e10fbc896f199425afe96
careless-coder95 <dhananjaymaurya3000@gmail.com>
2026-05-22 19:59:50 +0530

    Create __init__.py

---
+++ modules/__init__.py
@@ -0,0 +1,25 @@
+"""
+Modules Package
+Imports all command modules to register handlers
+"""
+
+# Import all modules to register handlers
+from . import help
+from . import voice
+from . import admin
+from . import record
+from . import screenshare
+from . import tools
+from . import settings
+
+__all__ = [
+    'help',
+    'voice',
+    'admin',
+    'record',
+    'screenshare',
+    'tools',
+    'settings',
+]
+
+#tari ma ki chut
